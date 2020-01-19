11dcc51d5ab8016398229d71bb19637f7489b9f2
stralar <larsstratmann@gmx.de>
2020-01-19 21:53:44 +0100

    progarm and libarys added

--- Programs/Tuerschloss.StepperNummernblock/configuration.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONFIGURATION_h
-#define CONFIGURATION_h
-
-#define password "123"
-
-#endif
--- Tuerschloss.ServoNummernblock/configuration.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIGURATION_h
-#define CONFIGURATION_h
-
-#define password "0815"
-#define servoOpen 90 //Grad
-
-
-#endif
